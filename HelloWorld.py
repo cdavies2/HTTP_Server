9a6a847bb20b0704f02a513b8980c01e8547f06d
cdavies2 <cdavies@student.sjny.edu>
2024-06-07 12:21:42 -0400

    Added post route and pokemon directory

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
 app=Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
         if request.method=='GET':
-              return "<p>Hello, World!</p>"
+              return "Hello, World!"
+        if request.method=='POST':
+              return request.get_data()
+
+@app.route('/pokemon/<type>', methods=['GET'])
+def pokemon(type):
+       if request.method=='GET':
+              return 'https://pokeapi.co/api/v2/pokemon/' + type + "  "
